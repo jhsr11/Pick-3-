e3990ff194603a08399c3d022344908abde5c8c6
jhsr11 <j.hollowaysr.11@gmail.com>
2026-01-13 15:34:56 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "pik3-grid-v2";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
